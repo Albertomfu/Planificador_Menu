9e8ddcfe54de2c5cfc1abd49dc968c39cc0e8f33
Alberto Molina <albertomolinadelafuente@gmail.com>
2025-01-20 13:19:29 +0100

    arreglos

--- email-server/server.js
+++ email-server/server.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
